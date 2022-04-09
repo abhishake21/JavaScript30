a6cc382fcb888962b4155cb4ec90634d5694cefd
abhishake21 <y.abhishake210@gmail.com>
2022-04-09 19:41:39 +0530

    added click event for mobile

--- 01 - Drum Kit/app.js
+++ 01 - Drum Kit/app.js
@@ -15,3 +15,64 @@ function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
+
+
+
+// For Touch devices
+
+const divA = document.querySelector('#A');
+const divS = document.querySelector('#S');
+const divD = document.querySelector('#D');
+const divF = document.querySelector('#F');
+const divG = document.querySelector('#G');
+const divH = document.querySelector('#H');
+const divJ = document.querySelector('#J');
+const divK = document.querySelector('#K');
+const divL = document.querySelector('#L');
+
+
+divA.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="65"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divS.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="83"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divD.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="68"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divF.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="70"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divG.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="71"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divH.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="72"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divJ.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="74"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divK.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="75"]');
+    audio.currentTime = 0;
+    audio.play();
+});
+divL.addEventListener('click', ()=>{
+    const audio = document.querySelector('audio[data-key="76"]');
+    audio.currentTime = 0;
+    audio.play();
+});
