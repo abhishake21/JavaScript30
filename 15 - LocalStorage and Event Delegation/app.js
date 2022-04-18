76d3fff83b6da124463a3fb8f538a0beb6bafa1c
abhishake21 <y.abhishake210@gmail.com>
2022-04-18 19:55:31 +0530

    done

--- 15 - LocalStorage and Event Delegation/app.js
+++ 15 - LocalStorage and Event Delegation/app.js
@@ -1,17 +1,73 @@
-const input = document.querySelector('input[type=text]');
 const form = document.querySelector('.add-items');
-const playersList = document.querySelector('.players')
+const playersList = document.querySelector('.players');
+
+const deleteBtn = document.querySelector('.delete-all');
+
+const players = JSON.parse(localStorage.getItem('players')) || [];
 
 function addPlayer(e){
     e.preventDefault();
-    let output = `
-    <div class="player">
-        <img class="logo" src="https://img.icons8.com/emoji/48/000000/soccer-ball-emoji.png"/>
-        <li>${input.value}</li>
-    </div>
-    `
-    playersList.innerHTML += output;
-    input.value = '';
+    const input = this.querySelector('input[name=player]');
+    const player = {
+        text: input.value,
+        done: false
+    }
+
+    players.push(player);
+    populateList(players, playersList);
+    localStorage.setItem('players', JSON.stringify(players));
+    this.reset();
 }
 
-form.addEventListener('submit', addPlayer)
+function populateList(players=[], playersList){
+    playersList.innerHTML = players.map((player, i) => {
+        return `
+        <li class="player">
+            <input type="checkbox" data-index=${i} id="item${i}" ${player.done ? 'checked' : ''}/>
+            <label for="item${i}">${player.text}</label>
+        </li>
+        `;
+    }).join('');
+
+    if(players.length===0){
+        deleteBtn.style.display = 'none';
+    } else {
+        deleteBtn.style.display = '';
+    }
+}
+
+function toggleDone(e){
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    players[index].done = !players[index].done;
+
+    localStorage.setItem('players', JSON.stringify(players));
+    populateList(players, playersList);
+}
+
+function deleteAll(){
+    const allPlayers = document.querySelectorAll('.player');
+    allPlayers.forEach(player => {
+        player.remove();
+    })
+    players.splice(0, players.length);
+    localStorage.removeItem('players');
+
+    if(players.length===0){
+        deleteBtn.style.display = 'none';
+    } else {
+        deleteBtn.style.display = '';
+    }
+}
+
+
+
+
+form.addEventListener('submit', addPlayer);
+playersList.addEventListener('click', toggleDone);
+
+deleteBtn.addEventListener('click', deleteAll);
+
+populateList(players, playersList);
+
