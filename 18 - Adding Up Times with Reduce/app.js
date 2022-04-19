ebfb121df941b42b33a8f0bb1780d630329a679f
abhishake21 <y.abhishake210@gmail.com>
2022-04-19 15:30:28 +0530

    adding times done

---
+++ 18 - Adding Up Times with Reduce/app.js
@@ -0,0 +1,27 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins*60) + secs;
+    })
+    .reduce((total, vidSeconds) => total + vidSeconds)
+    
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft/60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
+
+// 
+
+const ul = document.querySelector('.videos');
+let output = `
+<li>Total video time - <span class="total-time">${hours}:${mins}:${secondsLeft}</span></li>
+`
+ul.innerHTML += output;
