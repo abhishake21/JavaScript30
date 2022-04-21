e30c54c73ca96c0f298bedc8aee39114d423fc62
abhishake21 <y.abhishake210@gmail.com>
2022-04-21 16:41:01 +0530

    events done

---
+++ 25 - Event Capture, Propagation, Bubbling and Once/app.js
@@ -0,0 +1,14 @@
+const divs = document.querySelectorAll('div');
+
+function logText(e){
+    console.log(this.classList.value);
+    // e.stopPropagation();
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    once: true
+}));
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => alert("Hello 🌎..."), {once:true})
