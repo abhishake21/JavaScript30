210b49a2d010fa395b3dcfd5d376c87c18fca186
abhishake21 <y.abhishake210@gmail.com>
2022-04-23 20:20:16 +0530

    countdown clock done

---
+++ 29 - Countdown Clock/app.js
@@ -0,0 +1,54 @@
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+let countDown;
+
+function timer(seconds){
+    clearInterval(countDown)
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countDown = setInterval(()=>{
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft<0) {
+            clearInterval(countDown);
+            return;
+        };
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remaninderSeconds = seconds % 60
+    
+    const display = `${minutes}:${remaninderSeconds < 10 ? '0' : ''}${remaninderSeconds}`;
+
+    timerDisplay.textContent = display
+}
+
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hours = end.getHours()
+    const minutes = end.getMinutes()
+    endTime.textContent = `Be back at ${hours>12 ? hours-12 : hours}:${minutes<10 ? '0':''}${minutes}`
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(btn => btn.addEventListener('click', startTimer))
+document.customForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins*60);
+    this.reset();
+})
